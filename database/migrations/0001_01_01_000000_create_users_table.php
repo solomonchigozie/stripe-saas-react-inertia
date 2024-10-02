d1852abfec047adeebf54934668907d30727888c
solomon chigozie <solomon1chigozie@gmail.com>
2024-10-02 06:12:22 +0100

    observer added and migration updated

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Observers\UserObserver;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -16,6 +18,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('available_credits');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Feature;
+use App\Models\Package;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -18,6 +20,45 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password'=>bcrypt('12345678'),
         ]);
+
+        Feature::create([
+            'image'=>'https://img.icons8.com/?size=100&id=63650&format=png&color=000000',
+            'route_name'=>'feature1.index',
+            'name'=>'Calculate Sum',
+            'description'=>'Calculate sum of two numbers',
+            'required_credits' => 1,
+            'active'=>true
+        ]);
+
+        Feature::create([
+            'image'=>'https://img.icons8.com/?size=100&id=80413&format=png&color=000000',
+            'route_name'=>'feature2.index',
+            'name'=>'Calculate difference',
+            'description'=>'Calculate difference of two numbers',
+            'required_credits' => 3,
+            'active'=>true
+        ]);
+
+        Package::create([
+            'name'=>'Basic',
+            'price'=>5,
+            'credits'=> 20
+        ]);
+
+        Package::create([
+            'name'=>'Silver',
+            'price'=>20,
+            'credits'=>100,
+        ]);
+
+        Package::create([
+            'name'=>'Gold',
+            'price'=>50,
+            'credits'=>500,
+        ]);
+
+
     }
 }
