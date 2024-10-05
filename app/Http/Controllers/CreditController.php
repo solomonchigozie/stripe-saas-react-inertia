730b3aac0d7e2b680eca44e5692d132176f1a4f1
solomon chigozie <solomon1chigozie@gmail.com>
2024-10-05 13:12:59 +0100

    update to stripe

--- app/Http/Controllers/CreditController.php
+++ app/Http/Controllers/CreditController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Package;
 use Illuminate\Http\Request;
 
 class CreditController extends Controller
 {
-    //
+    public function index(){
+
+    }
+
+    public function buyCredit(Package $package){
+
+    }
+
+    public function success(){
+
+
+    }
+
+    public function cancel(){
+
+    }
+
+    public function webhook(){
+        
+    }
 }
